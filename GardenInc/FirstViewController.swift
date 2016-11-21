8f524691f465edc12c4a073b073f661613d8c734
TreQues <terrell@flygoody.com>
2016-11-21 15:11:08 -0500

    Initial Commit

---
+++ GardenInc/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GardenInc
+//
+//  Created by Terrell Robinson on 11/21/16.
+//  Copyright © 2016 FlyGoody. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
